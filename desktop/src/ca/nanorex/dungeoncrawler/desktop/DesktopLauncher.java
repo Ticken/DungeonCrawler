efe0a1187ad700b278eada11844d1bd0fbb82f6e
NanoRex <djlneuman@gmail.com>
2018-10-25 23:00:59 -0400

    Added preliminary desktop icon, Android app now hides status bar and does not request sensor data

--- desktop/src/ca/nanorex/dungeoncrawler/desktop/DesktopLauncher.java
+++ desktop/src/ca/nanorex/dungeoncrawler/desktop/DesktopLauncher.java
@@ -4,9 +4,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import ca.nanorex.dungeoncrawler.DungeonCrawler;
 
+import static com.badlogic.gdx.Files.FileType;
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Crawler";
+		config.addIcon("icons/icon_desktop_128.png", FileType.Internal);
+		config.addIcon("icons/icon_desktop_32.png", FileType.Internal);
 		new LwjglApplication(new DungeonCrawler(), config);
 	}
 }
